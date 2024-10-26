184930cb517903b6af747bc7f8e7fc93a3f775f8
shivanshthakur4u <boyslike2play@gmail.com>
2024-10-26 18:42:58 +0530

    few fixes in forgot apssword

--- controller/userController.js
+++ controller/userController.js
@@ -111,9 +111,10 @@ export const forgotPassword = async (req, res) => {
       `
     };
     const transporter = nodemailer.createTransport({
-       host: "smtp.gmail.com",
+      host: "smtp.ethereal.email",
+      Port:587,
+      secure: false, 
       auth: {
-        type: "login",
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
       }
