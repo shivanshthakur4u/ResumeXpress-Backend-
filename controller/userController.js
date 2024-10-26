e839ec321b418489c2a1779de3092f66f298cea0
shivanshthakur4u <boyslike2play@gmail.com>
2024-10-26 18:53:57 +0530

    noidemailer port fixes

--- controller/userController.js
+++ controller/userController.js
@@ -110,10 +110,12 @@ export const forgotPassword = async (req, res) => {
         <p>If you didn't request this, please ignore this email.</p>
       `
     };
+
+    // console.log("password:",process.env.EMAIL_APP_PASSWORD )
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 465,
-      secure: true,
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
